0d6cd43f2bb67973c1a85672acb590242102de52
davegord <davegord@redhat.com>
2022-02-05 08:15:20 -0500

    you're doing great exercies 2 and 3

---
+++ section2/handon2.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+var x int
+var y string
+var z bool
+
+func main() {
+	fmt.Println(x, y, z)
+	fmt.Println("This is x", x)
+	fmt.Println("This is y", y)
+	fmt.Println("This is z", z)
+}
